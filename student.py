a2f47a3b3ea3eb5f7828ece57f6f26bb3682d6ec
KphMickael <mickael.phung@saintmichelannecy.fr>
2024-11-03 09:49:58 +0100

    amélioration script avec pylint et black

--- student.py
+++ student.py
@@ -1,14 +1,16 @@
 class NoteInvalide(Exception):
     pass
 
+
 class Eleve:
     def __init__(self):
-        self.notes=[]
-        self.moyenne=0
+        self.notes = []
+        self.moyenne = 0
 
-    def ajouter_note(self,note):
-        if note<0 or note>20: 
+    def ajouter_note(self, note):
+        if note < 0 or note > 20:
             raise NoteInvalide("La note doit être entre 0 et 20.")
         self.notes.append(note)
-        self.moyenne=sum(self.notes)/len(self.notes)
+        self.moyenne = sum(self.notes) / len(self.notes)
+
 
