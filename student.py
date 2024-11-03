ee9e6ce0851e6f97125d7828f084999333344f4a
KphMickael <mickael.phung@saintmichelannecy.fr>
2024-11-03 08:20:52 +0100

    first commit

--- student.py
+++ student.py
@@ -1,16 +1,18 @@
 class Eleve:
     def __init__(self):
-        self.notes = []  # Liste pour stocker les notes
-        self.moyenne = 0  # La moyenne commence à 0
+        self.notes=[]
+        self.moyenne=0 
 
-    def ajouter_note(self, note):
-        # Vérifie que la note est entre 0 et 20
-        if note < 0 or note > 20:
+    def ajouter_note(self,note):
+
+        
+        if note<0 or note>20:
             raise NoteInvalide("La note doit être entre 0 et 20.")
         
-        # Ajoute la note et met à jour la moyenne
+
         self.notes.append(note)
-        self.moyenne = sum(self.notes) / len(self.notes)
+        self.moyenne=sum(self.notes) / len(self.notes)
 
 class NoteInvalide(Exception):
-    pass  # Exception si la note n'est pas valide
+    
+    pass
