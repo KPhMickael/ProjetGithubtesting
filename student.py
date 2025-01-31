ad2b44aa042aa9d89e86ff668039276b9d836f86
arnaudg74 <arnaud.guttin@saintmichelannecy.fr>
2025-01-31 11:08:34 +0100

    Update student.py

--- student.py
+++ student.py
@@ -2,18 +2,13 @@ class NoteInvalide(Exception):
     """Exception levée lorsque la note fournie est invalide."""
     pass
 
-
 class Eleve:
-    """Représente un élève et gère ses notes.
+    """Représente un élève et gère ses notes."""
 
-    Attributes:
-        notes (list): Une liste des notes de l'élève.
-        moyenne (float): La moyenne des notes de l'élève.
-    """
     def __init__(self):
         """Initialise un nouvel élève avec une liste de notes vide et une moyenne de 0."""
         self.notes = []
-        self.moyenne = 0
+        self.moyenne = 0.0
 
     def ajouter_note(self, note):
         """Ajoute une note à l'élève et met à jour la moyenne.
@@ -24,8 +19,8 @@ class Eleve:
         Raises:
             NoteInvalide: Si la note est inférieure à 0 ou supérieure à 20.
         """
-        if note < 0 or note > 20:
+        if not (0 <= note <= 20):
             raise NoteInvalide("La note doit être entre 0 et 20.")
+        
         self.notes.append(note)
         self.moyenne = sum(self.notes) / len(self.notes)
-
